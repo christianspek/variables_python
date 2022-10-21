e4c6b7aeafc6349bfe66b1512f05d6bc65deafdf
christianspek <115673694+christianspek@users.noreply.github.com>
2022-10-21 09:56:33 -0300

    Add files via upload

--- ejercicios_practica/ejercicio_1.py
+++ ejercicios_practica/ejercicio_1.py
@@ -16,14 +16,20 @@ numero_2 = 7
 # Crear una variable llamada suma donde se 
 # almacene el valor de la suma de las variable numero_1 y numero_2
 # ej:
-# suma = .....
+# suma = ..
 
+suma = numero_1 + numero_2
 # Imprimir en pantalla el resultado de la suma
 # print(....)
+
+
+print(suma)
 
 # Repita el procedimiento para realizar la resta
 # Crear una variable llamada resta donde se 
 # almacene el valor de la resta de numero_1 menos numero_2
 
+resta = numero_1 - numero_2
+print(resta)
 
 # Imprimir en pantalla el resultado de la resta
